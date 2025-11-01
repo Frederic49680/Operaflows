fde931579ed616e98b151873aee7ed7905709e89
Baudry Frederic <fredericbaudry49680@gmail.com>
2025-11-01 12:49:18 +0100

    fix: Correction balise div manquante dans formulaire edition

--- src/app/rh/[id]/collaborateur-detail-client.tsx
+++ src/app/rh/[id]/collaborateur-detail-client.tsx
@@ -545,6 +545,7 @@ export default function CollaborateurDetailClient({
                 <Phone className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
               </div>
             </div>
+            </div>
           </div>
 
           {/* Section Informations professionnelles */}
